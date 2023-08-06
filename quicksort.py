b9da477a57827d823917f5106ec61e1b593461df
Daniel De la Cruz <danieldelagavarain.dev@gmail.com>
2023-08-06 06:27:23 -0700

    Random pivot implemented in quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,10 +1,15 @@
+from random import randrange
+
 def quicksort(array):
     '''Given an array, its sorted in an ASC fashion using recursion.'''
     if (len(array) < 2):
         return array
     
-    pivot = array[0]
-    elementsLowerThanPivot = [i for i in array[1:] if i <= pivot]
-    elementsHigherThanPivot = [i for i in array[1:] if i >= pivot]
+    randomIndex = randrange(len(array))
+    pivot = array[randomIndex]
+
+    arrayWithoutPivot = array[:randomIndex] + array[randomIndex+1:]
+    elementsLowerThanPivot = [i for i in arrayWithoutPivot if i <= pivot]
+    elementsHigherThanPivot = [i for i in arrayWithoutPivot if i >= pivot]
 
     return quicksort(elementsLowerThanPivot) + [pivot] + quicksort(elementsHigherThanPivot)
